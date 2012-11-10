67a8a1625dd6c6ac84d810ac8517111af8c5d6ac
Felipe Munhoz <felipe@felipemunhoz.com>
2012-11-10 16:36:40 -0200

    Set canvas dimensions to window dimensions

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -6,7 +6,7 @@ var Renderer = function (options) {
   that.onRender = options.onRender;
   that.world = options.world;
 
-  that.canvas = new Canvas(document.body, 2000, 2000);
+  that.canvas = new Canvas(document.body, window.innerWidth, window.innerHeight);
   that.canvas.addFrameListener(function (t, dt) {
     that.onRender(t, dt);
     that.update(t, dt);
